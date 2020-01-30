2e20e9bb7ff0bb4f8df26e9f2fc553080c772102
TodoreTu <TodoreTu>
2020-01-31 00:05:07 +0200

    fixed .gitignore

--- structural-design-patterns/adapter-demo/Image.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Image = /** @class */ (function () {
-    function Image() {
-    }
-    return Image;
-}());
-exports.Image = Image;
